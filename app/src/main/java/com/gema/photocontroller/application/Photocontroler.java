e232de641b41241f930a35af02b2d72bdaca7d5e
Fedor Endovitskiy <rusfjord@ulteam8.ru>
2016-12-25 11:16:12 +0300

    Prepared by a single point of access to the database

--- app/src/main/java/com/gema/photocontroller/application/Photocontroler.java
+++ app/src/main/java/com/gema/photocontroller/application/Photocontroler.java
@@ -2,20 +2,38 @@ package com.gema.photocontroller.application;
 
 import android.app.Application;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Typeface;
 
 import com.gema.photocontroller.commons.SettingsHelper;
+import com.gema.photocontroller.db.SQLHelper;
 
 public class Photocontroler extends Application {
+
+    private static SQLHelper dbHelper;
+    private static SQLiteDatabase db;
+
     @Override
     public void onCreate() {
         super.onCreate();
-        SettingsHelper.initInstance(getApplicationContext());
+        SettingsHelper.initInstance(this);
+        setDb();
+
+    }
+
+    private void setDb() {
+        dbHelper = new SQLHelper(this);
+        db = dbHelper.getWritableDatabase();
     }
 
     public static Typeface getFont(Context context) {
         String fontPath = "fonts/Roboto-Light.ttf";
         return Typeface.createFromAsset(context.getAssets(), fontPath);
+    }
+
+    public static SQLiteDatabase getDb() {
+        return db;
     }
 
 }
