2fc239217028dfea5bca262bd226c8e255ecc5f0
Fedor Endovitskiy <rusfjord@ulteam8.ru>
2016-12-25 12:29:03 +0300

    Created method which get md5

--- app/src/main/java/com/gema/photocontroller/application/Photocontroler.java
+++ app/src/main/java/com/gema/photocontroller/application/Photocontroler.java
@@ -9,6 +9,10 @@ import android.graphics.Typeface;
 import com.gema.photocontroller.commons.SettingsHelper;
 import com.gema.photocontroller.db.SQLHelper;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Photocontroler extends Application {
 
     private static SQLHelper dbHelper;
@@ -34,6 +38,25 @@ public class Photocontroler extends Application {
 
     public static SQLiteDatabase getDb() {
         return db;
+    }
+
+    public static String getMD5EncryptedString(String encTarget){
+        MessageDigest mdEnc = null;
+        try {
+            mdEnc = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Exception while encrypting to md5");
+            e.printStackTrace();
+        } // Encryption algorithm
+        String md5 = null;
+        if (mdEnc != null) {
+            mdEnc.update(encTarget.getBytes(), 0, encTarget.length());
+            md5 = new BigInteger(1, mdEnc.digest()).toString(16);
+            while ( md5.length() < 32 ) {
+                md5 = "0"+md5;
+            }
+        }
+        return md5;
     }
 
 }
