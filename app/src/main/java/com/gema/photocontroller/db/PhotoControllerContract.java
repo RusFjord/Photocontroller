dd5cee07bde7151bc05c46f50e3cd396666a8c3b
Fedor Endovitskiy <rusfjord@ulteam8.ru>
2016-12-26 06:12:20 +0300

    Prepared class for sql table

--- app/src/main/java/com/gema/photocontroller/db/PhotoControllerContract.java
+++ app/src/main/java/com/gema/photocontroller/db/PhotoControllerContract.java
@@ -5,6 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
 
 import com.gema.photocontroller.application.Photocontroler;
+import com.gema.photocontroller.models.JournalRecord;
 import com.gema.photocontroller.models.PlaceForAds;
 import com.gema.photocontroller.models.Problems;
 import com.gema.photocontroller.models.Stations;
@@ -16,7 +17,7 @@ public final class PhotoControllerContract {
 
     public PhotoControllerContract() {}
 
-    static final class JournalEntry implements BaseColumns {
+    public static final class JournalEntry implements BaseColumns {
 
         public final static String TABLE_NAME = "journal";
 
@@ -25,9 +26,60 @@ public final class PhotoControllerContract {
         public final static String COLUMN_IS_SEND = "is_send";
         public final static String COLUMN_TYPE = "type";
         public final static String COLUMN_STATION = "station";
+        public final static String COLUMN_PLACEFORADS = "placeforads";
         //public final static String COLUMN_WAGON = "wagon";
         public final static String COLUMN_PROBLEM = "problem";
         public final static String COLUMN_COMMENT = "comment";
+
+        public static JournalRecord getOneEntry(int id) {
+            SQLiteDatabase db = Photocontroler.getDb();
+            String[] projection = {
+                    _ID,
+                    COLUMN_DATE,
+                    COLUMN_IS_SEND,
+                    COLUMN_TYPE,
+                    COLUMN_COMMENT,
+                    COLUMN_PROBLEM,
+                    COLUMN_PLACEFORADS,
+                    COLUMN_STATION};
+            String selection = _ID + " = ?";
+            String[] selectionArgs = {String.valueOf(id)};
+            JournalRecord journalRecord = null;
+            try (Cursor cursor = db.query(
+                    TABLE_NAME,   // таблица
+                    projection,            // столбцы
+                    selection,                  // столбцы для условия WHERE
+                    selectionArgs,                  // значения для условия WHERE
+                    null,                  // Don't group the rows
+                    null,                  // Don't filter by row groups
+                    null)) {
+
+                if (cursor.moveToNext()) {
+
+                    journalRecord = new JournalRecord(cursor);
+                }
+            }
+            return journalRecord;
+        }
+
+        public static ArrayList<JournalRecord> getAllEntries() {
+            SQLiteDatabase db = Photocontroler.getDb();
+            ArrayList<JournalRecord> list = new ArrayList<>();
+            try (Cursor cursor = db.query(TABLE_NAME, null, null, null, null, null, null)) {
+//                int idColumnIndex = cursor.getColumnIndex(_ID);
+//                int nameColumnIndex = cursor.getColumnIndex(COLUMN_NAME);
+//                int codeColumnIndex = cursor.getColumnIndex(COLUMN_CODE);
+
+                while (cursor.moveToNext()) {
+                    // Используем индекс для получения строки или числа
+//                    int currentID = cursor.getInt(idColumnIndex);
+//                    String currentName = cursor.getString(nameColumnIndex);
+//                    String currentCode = cursor.getString(codeColumnIndex);
+                    list.add(new JournalRecord(cursor));
+                }
+            }
+            return list;
+        }
     }
 
     public static final class ProblemsEntry implements BaseColumns {
