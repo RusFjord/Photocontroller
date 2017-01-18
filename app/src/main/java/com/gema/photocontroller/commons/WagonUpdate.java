357fb306f2bfd4c289f8fb718189e5d9143eacb5
Fedor Endovitskiy <rusfjord@ulteam8.ru>
2017-01-18 06:04:52 +0300

    Bugfix

--- app/src/main/java/com/gema/photocontroller/commons/WagonUpdate.java
+++ app/src/main/java/com/gema/photocontroller/commons/WagonUpdate.java
@@ -5,27 +5,27 @@ import android.util.Log;
 
 import com.gema.photocontroller.db.PhotoControllerContract;
 import com.gema.photocontroller.db.UpdateDbTable;
-import com.gema.photocontroller.models.WagonType;
+import com.gema.photocontroller.models.Wagon;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class WagonTypeUpdate extends UpdateDbTable {
+public class WagonUpdate extends UpdateDbTable {
 
     @Override
     protected void updateTable(SQLiteDatabase db, String fileList) {
-        dropDb(db, PhotoControllerContract.WagonTypeEntry.TABLE_NAME);
+        dropDb(db, PhotoControllerContract.WagonEntry.TABLE_NAME);
         try {
             JSONObject dataJson = new JSONObject(fileList);
-            JSONArray jsonArray = dataJson.getJSONArray("wagontypes");
+            JSONArray jsonArray = dataJson.getJSONArray("wagons");
             for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject wagonTypeJson = jsonArray.getJSONObject(i);
-                WagonType currentWagonType = new WagonType(wagonTypeJson);
-                currentWagonType.putDb(db);
+                JSONObject wagonJson = jsonArray.getJSONObject(i);
+                Wagon currentWagon = new Wagon(wagonJson);
+                currentWagon.putDb(db);
             }
 
         } catch (Exception e) {
-            Log.e("WAGON TYPE UPDATE", e.getMessage());
+            Log.e("WAGON UPDATE", e.getMessage());
         }
     }
 }
