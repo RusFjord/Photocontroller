0b2da5c79a2ee0046c5135f0ee05ba91e8c8a316
Fedor Endovitskiy <rusfjord@ulteam8.ru>
2017-01-18 05:56:36 +0300

    In class WorkFiles added method which set filename

--- app/src/main/java/com/gema/photocontroller/files/WorkFiles.java
+++ app/src/main/java/com/gema/photocontroller/files/WorkFiles.java
@@ -5,18 +5,13 @@ import android.util.Log;
 
 import com.gema.photocontroller.R;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.StringReader;
 
 public class WorkFiles {
 
@@ -25,6 +20,8 @@ public class WorkFiles {
     public WorkFiles(String filename) {
         this.filename = filename;
     }
+
+
 
     protected String ReadFile(Context context) {
 
@@ -51,42 +48,6 @@ public class WorkFiles {
         return result;
     }
 
-//    protected File ReadFileBinary(Context context) {
-//
-//        File file = new File(context.getApplicationInfo().dataDir + File.separatorChar + this.filename);
-//        int size = (int) file.length();
-//        byte[] bytes = new byte[size];
-//
-////int size = (int) file.length();
-////byte[] bytes = new byte[size];
-//try {
-//    BufferedInputStream buf = new BufferedInputStream(new FileInputStream(file));
-//    buf.read(bytes, 0, bytes.length);
-//    buf.close();
-////
-//////        String result = null;
-//////        BufferedReader bufferedReader = null;
-//////        String currentFileName = context.getApplicationInfo().dataDir + File.separatorChar + this.filename;
-////        try {
-////            FileReader fileReader = new FileReader(currentFileName);
-////            if (fileReader != null) {
-////                bufferedReader = new BufferedReader(fileReader);
-////                String line;
-////                StringBuilder stringBuilder = new StringBuilder();
-////                while ((line = bufferedReader.readLine()) != null) {
-////                    stringBuilder.append(line);
-////                }
-////                fileReader.close();
-////                result = stringBuilder.toString();
-////            }
-//        } catch (FileNotFoundException e) {
-//            Log.e("file_read", "File " + filename + " not found", e);
-//        } catch (IOException e) {
-//            Log.e("file_read", "IO error", e);
-//        }
-//        return result;
-//    }
-
     protected String WriteFile(Context context, String filename, String data) {
         String error = null;
         BufferedWriter bufferedWriter = null;
@@ -106,6 +67,10 @@ public class WorkFiles {
             }
         }
         return error;
+    }
+
+    protected void setFilename(String filename) {
+        this.filename = filename;
     }
 
 }
